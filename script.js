c7e40c6a8d0a318ef63af094064201775312d4f4
minhazahmmed <minhazahmmed231@gmail.com>
2025-08-30 15:44:31 +0600

    copy functionalities done

--- script.js
+++ script.js
@@ -21,11 +21,55 @@ for(let cardHeart of cardHearts){
 
 // call functionalities
 
-const callBtns = document.querySelectorAll(".card-call")
-for(let callBtn of callBtns){
-    Alert()
+const mainCoin = getElement("navbar-coin");
+const cardCalls = document.getElementsByClassName("card-call");
+for(let cardCall of cardCalls){
+    cardCall.addEventListener("click",function(){
+        const cardService = cardCall.parentNode.parentNode.children[1].children[1].innerText;
+
+        const cardNumber = cardCall.parentNode.parentNode.children[1].children[2].innerText;
+        console.log(cardNumber)
+
+       
+        const currentCoin = Number(mainCoin.innerText);
+      
+         const coinUpdate = currentCoin-20;
+
+          if(coinUpdate<0){
+            alert("❌You don't have enough coins. You need at least 20 coins to make a call.");
+            return;
+        }
+
+      alert(`📞Calling ${cardService} ${cardNumber}`); 
+
+         mainCoin.innerText = coinUpdate;
+
+    })
 }
 
+
+// copy functionalities
+
+const navBarCopy = getElement("navbar-copy");
+let copyCount = 0;
+const cardCopies = document.querySelectorAll(".card-copy");
+for(const cardCopy of cardCopies){
+    cardCopy.addEventListener('click',function(){
+        const getCopiedNumber = cardCopy.parentNode.parentNode.children[1].children[2].innerText;
+
+           const tempInput = document.createElement("textarea");
+        tempInput.value = getCopiedNumber;
+        document.body.appendChild(tempInput);
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+
+        alert(`Number is copied: ${getCopiedNumber}`);
+        
+        copyCount=copyCount+1;
+    navBarCopy.innerText = copyCount;
+    })
+}
 
 
 
